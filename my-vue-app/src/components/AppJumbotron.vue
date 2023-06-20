3230d26c7a1be2bb4937df9cd4b8550a42be2564
Anieb22 <anielloverdino@gmail.com>
2023-06-20 16:18:21 +0200

    aggiunto titolo e Jumbotron (esercizio concluso)

---
+++ my-vue-app/src/components/AppJumbotron.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <section>
+        <img src="../assets/img/jigar-panchal-ME1_AXA5lAM-unsplash.jpg" alt="">
+    </section>
+</template>
+
+<style>
+img {
+    width: 1200px;
+    margin: auto;
+}
+</style>
---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div>
+        <h1>la mia prima app con Vite</h1>
+    </div>
+</template>
+<style></style>
